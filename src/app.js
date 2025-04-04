9a994cfd795be342a5d6b5d549b3d20e00462f49
Vicente Contreras <vcontrerasalcu@gmail.com>
2025-04-04 07:45:01 +0200

    creacion clases

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import {express} from 'express';
+
+
+app = express();
+app.use(express.json());
+
+
+
---
+++ src/classes/Cliente.js
@@ -0,0 +1,13 @@
+
+class Cliente{
+    constructor(email, password, nombre, direccion, telefono, direccionEnvio){
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.direccionEnvio = direccionEnvio;
+    }
+}
+
+export default Cliente;
