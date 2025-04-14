63c1d420ce24bc42e93afd98467bf0648ff773eb
Vicente Contreras <vcontrerasalcu@gmail.com>
2025-04-14 05:17:32 +0200

    ruta productos configurada

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
-import {express} from 'express';
+import express from 'express';
+import rutaProductos from './routes/productos.js';
 
 
-app = express();
+const app = express();
 app.use(express.json());
+
+app.use('/productos',rutaProductos);
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`servidor escuchando en http://localhost:${PORT}..`));
+
 
 
 
--- src/classes/Productos.js
+++ src/classes/Productos.js
@@ -104,8 +104,6 @@ class Productos {
 
     }
 
-
-                        
-
-
 }
+
+export default Productos;
