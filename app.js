410937acd04a5fbd6f517eb950bac9bd3d02123c
Vicente Contreras <vcontrerasalcu@gmail.com>
2025-05-03 04:42:21 +0200

    fase 1 registro y cambio pass terminada

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ import rutaLogin from './routes/login.js';
 import rutaClientes from './routes/clientes.js';
 import  cors from 'cors';
 import path from 'path';
+import http from 'http';
+import {Server} from 'socket.io';
+
+
+
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000', 
+        methods: ['GET', 'POST']
+    }
+});
+
+
 
 app.use(express.static(path.join(process.cwd(), 'public')));
 app.use(express.json());
@@ -14,10 +28,10 @@ app.use(cors());
 
 app.use('/productos',rutaProductos);
 app.use('/login', rutaLogin);
-app.use('/clientes',rutaClientes);
+app.use('/clientes',rutaClientes(io));
 
 const PORT = 3001;
-app.listen(PORT, () => console.log(`servidor escuchando en http://localhost:${PORT}..`));
+server.listen(PORT, () => console.log(`servidor escuchando en http://localhost:${PORT}..`));
 
 
 
